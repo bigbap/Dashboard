09715052984c98baa296d958d9881fd0a8f6cec0
bigbap <simonrodri@mweb.co.za>
2019-03-31 11:25:35 +0100

    added ts script

---
+++ Dashboard/Scripts/Dashboard/Dashboard.ts
@@ -0,0 +1,22 @@
+﻿interface IViewParams {
+    templates?: string[]
+};
+
+export class Dashboard {
+    private static _instance: Dashboard;
+    private _viewParams: IViewParams;
+
+    private constructor(viewParams: IViewParams) {
+        this._viewParams = viewParams
+    }
+
+    public static GetInstance(viewParams: IViewParams) {
+        if (!this._instance) this._instance = new Dashboard(viewParams);
+
+        return this._instance;
+    }
+
+    public init() {
+        console.log("worked");
+    }
+}
